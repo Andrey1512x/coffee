78954b6d95b08d48a5356067bfbdbe31b2caf0c3
Redron <andr555111@gmail.com>
2021-12-25 19:22:17 +0300

    Espresso

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("main.ui", self)
+        self.load_table()
+        self.pushButton.clicked.connect(self.edit_item)
 
+    def load_table(self):
+        self.tableWidget.clear()
         connection = sqlite3.connect("coffee.sqlite")
         cursor = connection.cursor()
         result = cursor.execute("SELECT * FROM Coffee").fetchall()
@@ -17,12 +21,52 @@ class MyWidget(QMainWindow):
         title = ["id", "name", "roasting", "grain", "taste", "cost", "size"]
 
         self.tableWidget.setHorizontalHeaderLabels(title)
+        self.tableWidget.setRowCount(len(result))
         for i, row in enumerate(result):
-            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
             for j, elem in enumerate(row):
-                self.tableWidget.setItem(i, j, QTableWidgetItem(elem))
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
         self.tableWidget.resizeColumnsToContents()
 
+    def edit_item(self):
+        self.w = MyForm(self)
+        self.w.show()
+
+
+class MyForm(QMainWindow):
+    def __init__(self, parent):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.parent = parent
+        self.pushButton.clicked.connect(self.onClick)
+
+    def onClick(self):
+        connection = sqlite3.connect("coffee.sqlite")
+        cursor = connection.cursor()
+        id = self.idEdit.text()
+        name = self.nameEdit.text()
+        roasting = self.roastEdit.text()
+        grain = self.grainEdit.text()
+        taste = self.tasteEdit.text()
+        cost = self.costEdit.text()
+        size = self.sizeEdit.text()
+        grain = True if grain.lower() == "зерновой" else False
+        cost = int(cost)
+        size = int(size)
+        d = {"name": name, "roasting": roasting, "grain": grain,
+             "taste": taste, "cost": cost, "size": size}
+        if id:
+            cursor.execute(f"UPDATE Coffee SET name='{name}', roasting='{roasting}', grain={grain},"
+                           f"taste='{taste}', cost={cost}, size={size} WHERE id={int(id)}")
+        else:
+            id = cursor.execute("SELECT * FROM Coffee ORDER BY id DESC").fetchone()
+            id = 1 if not id else id[0] + 1
+            print(id)
+            cursor.execute(f"INSERT INTO Coffee(id, name, roasting, grain, taste, cost, size) "
+                           f"VALUES({id}, '{name}', '{roasting}', {grain}, '{taste}', {cost}, {size})")
+        connection.commit()
+        connection.close()
+        self.parent.load_table()
+        self.close()
 
 app = QApplication(sys.argv)
 ex = MyWidget()
